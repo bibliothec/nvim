ef950cda09e429dc239a6a09573dabfe9b4d99eb
bibliothec <mupimupicandy@gmail.com>
2023-05-07 13:42:11 +0900

    rm backup

--- init.lua
+++ init.lua
@@ -151,10 +151,6 @@ vim.api.nvim_create_autocmd({ "BufReadPost" }, {
  ]]
 
 --require('plugins')
-vim.cmd [[packadd packer.nvim]]
-return require('packer').startup(function()    
-    use('wbthomason/packer.nvim')
-end)
 
 require("packer").startup(function(use)
     -- powerline
